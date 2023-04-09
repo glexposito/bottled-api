7e63a390e616b29020bb966e6645ff1b6a852dd4
Guillermo <glexposito@gmail.com>
2023-04-09 19:56:37 +1200

    Add dummy endpoint filter (#4)

---
+++ src/Bottled.Api/Infrastructure/Filters/AddDummyHeaderFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace Bottled.Api.Infrastructure.Filters;
+
+public class AddDummyHeaderFilter : IEndpointFilter
+{
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        context.HttpContext.Response.Headers.Add("Dummy-Header", "I'm useless!");
+        var result = await next(context);
+        return result;
+    }
+}
